adc5534fad48662003aad6cd3b1db96a815f8fc4
Hironoiri Yamamoto <mr.nikoru918@gmail.com>
2020-02-22 16:57:03 +0900

    Fix restoring

--- scripts/lfs-restore.sh
+++ scripts/lfs-restore.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 CACHE_DIR=.lfs-caches
-LOCK_FILE=lfs-files.lock
 
-while read hash star filepath
-do  
-    dname=`dirname "${filepath}"`
-    cp -a "${CACHE_DIR}/${filepath}" "${dname}"
-done < lfs-files.lock
+rsync -a ${CACHE_DIR}/* $(pwd)
